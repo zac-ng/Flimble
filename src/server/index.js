4773f2229b4fc410cdaa1c929324993b302a4721
zac-ng <zncodebox@gmail.com>
2021-02-17 21:51:50 -0500

    Removed scripts and source html from webssh2 because it is incompatible in this repository. Reference pervious version or run scripts in webssh2 folder to build/edit.

--- src/server/index.js
+++ src/server/index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) =>{
     res.send("");
 })
 app.get('/bob', (req, res) =>{
-    ssh.connect('34.238.118.19')
+    ssh.connect('34.238.118.19')    //ssh allows us to pass in a variable, thus we can pass in a number. This can allow us to query the api and then pass it in to be connected.
     res.redirect('http://localhost:3113/ssh/host/127.0.0.1');
 })
 //ssh.connect('18.207.0.192'); //ssh allows us to pass in a variable, thus we can pass in a number. This can allow us to query the api and then pass it in to be connected.
