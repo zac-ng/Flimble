ca29eb8b6dfd32320ed8c9415425edaa485f2797
Zac <zncodebox@gmail.com>
2021-03-26 01:16:23 -0400

    Fixing untracked files

--- src/server/node_modules/snyk/dist/lib/options-validator.d.ts
+++
@@ -1,3 +0,0 @@
-import { MonitorOptions, Options, TestOptions } from './types';
-import { SupportedPackageManagers } from './package-managers';
-export declare function validateOptions(options: (Options & TestOptions) | (Options & MonitorOptions), packageManager?: SupportedPackageManagers): Promise<void>;
