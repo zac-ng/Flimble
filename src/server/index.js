8e79b3d18692685f48530a59a2d8a0e6a2992d95
Zac <zncodebox@gmail.com>
2021-04-12 20:09:21 -0400

    Fixed naming error in client.html preventing rendering

--- src/server/index.js
+++ src/server/index.js
@@ -7,13 +7,12 @@ const PORT = 2112
 
 const ssh = require(path.join(__dirname, 'ssh', 'ssh.js'))
 
-
 app.get('/', (req, res) =>{
     res.send("Index Page");
 })
 app.get('/bob', (req, res) =>
 {
-	const IP = '54.211.114.44'
+	const IP = '3.82.242.133'
 	ssh.connect(IP)	//ssh allows us to pass in a variable, thus we can pass in a number. This can allow us to query the api and then pass it in to be connected.
     console.log("Connected to " + IP + "\n Redirecting to terminal");
 	//res.redirect('http://75.101.232.49:3113/ssh/user');	//When running on EC2 redirect here.
