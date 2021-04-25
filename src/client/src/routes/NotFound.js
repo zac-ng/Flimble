512e5517ea5d1f471c1a86833b309c49a8e18e33
Zac <zncodebox@gmail.com>
2021-04-24 23:52:18 -0400

    Added 404 page

--- src/client/src/routes/NotFound.js
+++ src/client/src/routes/NotFound.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import error from '../static/404.jpg'
+
+const image = {
+    height:'50%',
+    width:'50%',
+    position: 'absolute', left: '50%', top: '40%',
+    transform: 'translate(-50%, -50%)'
+}
+
 export default function NotFound(){
+    
     return(
-        "This resource was not found."
+        <React.Fragment>
+            <img style={image} src={error} alt="404" />
+        </React.Fragment>
     )
 }
--- src/client/src/routes/SSH.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function SSH(){
-    return(
-        <React.Fragment>
-            Hello
-        </React.Fragment>
-    )
-}
